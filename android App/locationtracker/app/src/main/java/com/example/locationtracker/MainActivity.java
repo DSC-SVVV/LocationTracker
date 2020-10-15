e7682d787b5127d7da10125e9a4bd6e6326ed93e
Robin Singh Chouhan <robinsingh.02@gmail.com>
2020-10-15 17:55:25 +0530

    Added location feature which gives latitude and longitude.

--- android App/locationtracker/app/src/main/java/com/example/locationtracker/MainActivity.java
+++ android App/locationtracker/app/src/main/java/com/example/locationtracker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         att.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("STATUS","click!!");
                 startActivity(new Intent(MainActivity.this,MapsActivity.class));
             }
         });
